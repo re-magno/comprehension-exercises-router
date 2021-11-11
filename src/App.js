8c3c018001fb45020450b0aaf33f931aa6bb50b8
Renata <renatamagno@live.com>
2021-11-11 13:11:46 -0300

    feat: exercicio 5 - add rota para users

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Home from './components/Home';
 import About from './components/About';
+import Users from './components/Users';
 import { BrowserRouter } from 'react-router-dom';
 import { Route, Switch } from 'react-router-dom';
 
@@ -10,6 +11,7 @@ class App extends Component {
       <BrowserRouter>
         <Route path="/" exact component={Home} />
         <Route path="/about" exact component={About} />
+        <Route path="/users" exact component={Users} />
       </BrowserRouter>
     );
   }
