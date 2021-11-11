dd68f472c2b1a0518cef6955754c19f9c592c537
Renata <renatamagno@live.com>
2021-11-11 14:20:25 -0300

    chore: iniciando exercicio 8

--- src/App.js
+++ src/App.js
@@ -3,15 +3,20 @@ import Home from './components/Home';
 import About from './components/About';
 import Users from './components/Users';
 import { BrowserRouter } from 'react-router-dom';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Link } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Route path="/" exact component={Home} />
-        <Route path="/about" exact component={About} />
-        <Route path="/users" exact component={Users} />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/users" render={(props) => <Users {...props} users={ "Good Morning" } />} />
+        </Switch>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/users">Users</Link>
       </BrowserRouter>
     );
   }
