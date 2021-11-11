094b85dfb5f12da4b5c119a4f41e6a76d8385f8f
Renata <renatamagno@live.com>
2021-11-11 13:07:37 -0300

    feat: exercicios 1 a 4

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 import Home from './components/Home';
+import About from './components/About';
+import { BrowserRouter } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <Home />
+      <BrowserRouter>
+        <Route path="/" exact component={Home} />
+        <Route path="/about" exact component={About} />
+      </BrowserRouter>
     );
   }
 }
